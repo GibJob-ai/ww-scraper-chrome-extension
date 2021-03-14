bc0d58e96d6c68df884f6b77e0829456d382e614
Kaelan Moffett-Steinke <kaelan.ms@gmail.com>
2021-03-13 19:11:46 -0500

    init

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Installation
+* install npm if needed: https://www.npmjs.com/get-npm
+* install npm packages
+```
+npm install
+```
+* build using webpack
+```
+npm run build
+```
+# Add extension to Chrome
+* Head over to chrome://extensions
+* Toggle "Developer mode" on.
+* Click Load unpacked.
+* Upload the dist folder to chrome
+* The extension should now be available for use
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+function getXpath (headerText, lowerTable){
+  return `//strong[contains(text(), '${headerText}')]/parent::td/parent::tr/td[2]${lowerTable?"":"/span"}`;
+}
+
+function getElementByXpath(xpath) {
+  return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function sendMessage(message) {
+    chrome.runtime.sendMessage(message)
+}
+
+async function scrapeContent(){
+  const JOB_CATEGORY = "Job Category (NOC):";
+  const JOB_SUMMARY = "Job Summary:";
+  const JOB_RESPONSIBILITIES = "Job Responsibilities:";
+  const REQUIRED_SKILLS = "Required Skills:";
+  const DOCUMENTS_REQUIRED = "Documents Required:";
+  const JOB_TITLE = "Job Title:";
+  const ORGANIZATION = "Organization:";
+  const DIVISION = "Division:";
+
+  const TITLES_MAIN_TABLE = [JOB_TITLE,
+    JOB_CATEGORY,
+    JOB_SUMMARY,
+    JOB_RESPONSIBILITIES,
+    REQUIRED_SKILLS,
+    DOCUMENTS_REQUIRED,
+    JOB_TITLE];
+
+  const TITLES_LOWER_TABLE = [ORGANIZATION,
+                        DIVISION];
+  let payload = {}
+  TITLES_MAIN_TABLE.forEach(function (i) {
+    let element = getElementByXpath(getXpath(i, false));
+    payload[i] = element.innerText;
+  });
+  TITLES_LOWER_TABLE.forEach(function (i) {
+    let element = getElementByXpath(getXpath(i, true));
+    payload[i] = element.innerText;
+  });
+  try {
+    // TODO replace this random API I found with our backend when it gets made
+    // this random api call is just to prove that we have external connectivity thanks to the payload injection
+    const response = await axios.get("https://covid19.mathdro.id/api/countries/canada");
+    payload['res'] = response.data;
+    return JSON.stringify(payload);
+  } catch (error) {
+    let error_str = `${error.message}`;
+    payload['res_err'] = error_str;
+    return JSON.stringify(payload);
+  }
+}
+
+try {
+  scrapeContent().then(content => {
+    chrome.runtime.sendMessage(`content ${content}`);
+  })
+}
+catch (err){
+  chrome.runtime.sendMessage(`outer error ${err.message}`);
+}
